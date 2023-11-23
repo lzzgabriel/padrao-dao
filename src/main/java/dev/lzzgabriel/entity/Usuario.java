7883292647515f83896797f140c4685127b6e54f
lzzgabriel <96301579+lzzgabriel@users.noreply.github.com>
2023-11-23 17:18:57 -0300

    :label: Add entidade Usuario

---
+++ src/main/java/dev/lzzgabriel/entity/Usuario.java
@@ -0,0 +1,80 @@
+package dev.lzzgabriel.entity;
+
+import java.time.Instant;
+
+public class Usuario {
+
+	private Integer id;
+	private String nome;
+	private String email;
+	private String endereco;
+	private String numero;
+	private String municipio;
+	private String uf;
+	private Instant momentoCadastro;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getMunicipio() {
+		return municipio;
+	}
+
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public Instant getMomentoCadastro() {
+		return momentoCadastro;
+	}
+
+	public void setMomentoCadastro(Instant momentoCadastro) {
+		this.momentoCadastro = momentoCadastro;
+	}
+
+}
