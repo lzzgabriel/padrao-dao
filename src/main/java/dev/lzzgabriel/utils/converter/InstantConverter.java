d7ea3cc19e78218cc6f7240c3acb5b08951bf149
lzzgabriel <96301579+lzzgabriel@users.noreply.github.com>
2023-12-04 10:13:47 -0300

    Ajuste no InstantConverter

--- src/main/java/dev/lzzgabriel/utils/converter/InstantConverter.java
+++ src/main/java/dev/lzzgabriel/utils/converter/InstantConverter.java
@@ -1,6 +1,7 @@
 package dev.lzzgabriel.utils.converter;
 
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 import jakarta.faces.component.UIComponent;
@@ -14,13 +15,15 @@ public class InstantConverter implements Converter<Instant> {
 
 	@Override
 	public Instant getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
-		Instant obj = (Instant) DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").parse(value);
+		Instant obj = (Instant) DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")
+				.withZone(ZoneId.of("America/Sao_Paulo")).parse(value);
 		return obj;
 	}
 
 	@Override
 	public String getAsString(FacesContext context, UIComponent component, Instant value) throws ConverterException {
-		String str = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").format(value);
+		String str = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")
+				.format(value.atZone(ZoneId.of("America/Sao_Paulo")));
 		return str;
 	}
 
