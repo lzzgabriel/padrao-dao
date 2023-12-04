570924594b0217d6e678d54f1ddad2a106fa367b
lzzgabriel <96301579+lzzgabriel@users.noreply.github.com>
2023-12-04 09:08:06 -0300

    :hammer: Implementação do FacesConverter para a classe Instant

---
+++ src/main/java/dev/lzzgabriel/utils/converter/InstantConverter.java
@@ -0,0 +1,27 @@
+package dev.lzzgabriel.utils.converter;
+
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.ConverterException;
+import jakarta.faces.convert.FacesConverter;
+
+@FacesConverter(value = "instantConverter")
+public class InstantConverter implements Converter<Instant> {
+
+	@Override
+	public Instant getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
+		Instant obj = (Instant) DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").parse(value);
+		return obj;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Instant value) throws ConverterException {
+		String str = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").format(value);
+		return str;
+	}
+
+}
